cd3d0fed365e6e94fb6d7d0462a65a32845edab6
Theo Lemay <theol0403@gmail.com>
2023-10-26 21:41:02 -0400

    add linear search

--- gen.cjs
+++ gen.cjs
@@ -1,15 +1,15 @@
-const { readdirSync } = require('fs')
-const { writeFileSync } = require('fs-extra')
-const path = require('path')
+const { readdirSync } = require("fs");
+const { writeFileSync } = require("fs-extra");
+const path = require("path");
 
 async function main() {
-  const files = readdirSync('./fixtures')
-  const items = []
+  const files = readdirSync("./fixtures");
+  const items = [];
   for (let idx = 0; idx < files.length; idx++) {
-    if (idx == 0) continue
-    let file_a = files[idx - 1]
-    let file_b = files[idx]
-    const name = `${file_a} - ${file_b}`
+    if (idx == 0) continue;
+    let file_a = files[idx - 1];
+    let file_b = files[idx];
+    const name = `${file_a} - ${file_b}`;
     file_a = path.resolve(__dirname, "./fixtures", file_a);
     file_b = path.resolve(__dirname, "./fixtures", file_b);
     const item = `
@@ -20,8 +20,8 @@ async function main() {
       const text_b = (await readFile(file_b)).toString().replace(/\\r\\n/g, '\\n')
       await assertTextChange(text_a, text_b)
     })
-    `
-    items.push(item)
+    `;
+    items.push(item);
   }
 
   const code = `
@@ -45,24 +45,53 @@ interface DocumentChange {
   rangeLength: number
 }
 
+// given an array of line lengths, find the line number given a character position after a known start line.
+// return the line as well as the number of characters until the start of the line.
+function findLine(
+  lineLengths: number[],
+  pos: number,
+  startLine: number
+): [number, number] {
+  let line = startLine;
+  let char = 0;
+  while (line < lineLengths.length && char + lineLengths[line] < pos) {
+    char += lineLengths[line] + 1; // count the newline
+    line++;
+  }
+  return [line, char];
+}
+
+// fast-myers-diff accepts a raw 1D string, and outputs a list of operations to apply to the buffer.
+// However, we must compute the line and character positions of the operations ourselves.
+// Assuming the operations are sequential, we can keep a "cursor" which keeps a running total of the number of characters before a given line.
+// Then, given a character position, we can start counting from the cursor to find the line number, and the remainder is the character position on the line.
 function* calcDiffWithPosition(
   oldText: string,
   newText: string
 ): Generator<DocumentChange> {
-  const eol = '\\n';
-  const splitText = oldText.split(eol);
   const patch = calcPatch(oldText, newText);
+  const lineLengths = oldText.split("\\n").map((line) => line.length);
+  let cursor = new Position(0, 0);
   for (const [start, end, text] of patch) {
-    const lineStart = oldText.slice(0, start).split(eol).length - 1;
-    const lineEnd = oldText.slice(0, end).split(eol).length - 1;
-    const charStart =
-      start -
-      (lineStart > 0 ? splitText.slice(0, lineStart).join(eol).length + eol.length : 0);
-    const charEnd =
-      end - (lineEnd > 0 ? splitText.slice(0, lineEnd).join(eol).length + eol.length : 0);
+    const [lineStart, charToLineStart] = findLine(
+      lineLengths,
+      start - cursor.character,
+      cursor.line
+    );
+    const [lineEnd, charToLineEnd] = findLine(
+      lineLengths,
+      end - charToLineStart - cursor.character,
+      lineStart
+    );
+    const charStart = start - charToLineStart - cursor.character;
+    const charEnd = end - charToLineStart - charToLineEnd - cursor.character;
     const range = new Range(
       new Position(lineStart, charStart),
       new Position(lineEnd, charEnd)
+    );
+    cursor = new Position(
+      lineEnd,
+      charToLineStart + charToLineEnd + cursor.character
     );
     yield {
       range,
@@ -94,12 +123,12 @@ describe('Test', async () => {
     await commands.executeCommand('workbench.action.closeAllEditors');
   });
 
-  ${items.join('\n')}
+  ${items.join("\n")}
 
 });
-  `
+  `;
 
-  writeFileSync('./src/test/suite/main.test.ts', code)
+  writeFileSync("./src/test/suite/main.test.ts", code);
 }
 
-main()
+main();
